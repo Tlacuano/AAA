f060bd676b49ad91d6398b7fafde8be50f32bb70
Tlacuano <20213tn105@utez.edu.mx>
2023-06-05 09:46:25 -0600

    proyecto inicializado back

---
+++ aaa_001/src/main/java/tlacuano/aaa_001/Aaa001Application.java
@@ -0,0 +1,13 @@
+package tlacuano.aaa_001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aaa001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aaa001Application.class, args);
+	}
+
+}
---
+++ aaa_001/src/test/java/tlacuano/aaa_001/Aaa001ApplicationTests.java
@@ -0,0 +1,13 @@
+package tlacuano.aaa_001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aaa001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
